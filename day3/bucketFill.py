aac13376b50f9a13d648db6ba45efcb8c7a45d8f
Billy Rebecchi <b.rebecchi@newcastle.ac.uk>
2017-12-03 21:20:21 +0000

    Write recursive flood fill algorithm.

--- day3/bucketFill.py
+++ day3/bucketFill.py
@@ -1,6 +1,6 @@
 # This is a bucket fill algorithm for day 3 of Devember
 # Time started: 9:10pm
-# Time finished: 
+# Time finished: 9:20pm
 
 # Create some image data
 image = [
@@ -15,13 +15,13 @@ image = [
 ]
 
 # Function to draw an image
-def drawImage(i):
+def drawImage(img):
 	
 	# Empty line
 	print("")
 	
 	# For each row
-	for row in i:
+	for row in img:
 		
 		# Create an empty string
 		r = str()
@@ -33,5 +33,27 @@ def drawImage(i):
 		# Print the row
 		print(r)
 		
+# Create a function to allow recursive flood fill
+def floodFill(img, x, y, find, replace):
+	
+	# If the data at the coordinate is not the same as that to be replaced, return
+	if not img[y][x] == find:
+		return
+	
+	# Replace the data
+	img[y][x] = replace
+	
+	# Recursively fill
+	floodFill(img, x + 1, y, find, replace)
+	floodFill(img, x - 1, y, find, replace)
+	floodFill(img, x, y + 1, find, replace)
+	floodFill(img, x, y - 1, find, replace)
+		
+# Draw the image
+drawImage(image)
+
+# Flood fill
+floodFill(image, 2, 2, ".", "x")
+
 # Draw the image
 drawImage(image)
