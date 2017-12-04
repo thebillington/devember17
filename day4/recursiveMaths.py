557d2ca188cb348233588342803111b0ad7513f7
Billy Rebecchi <b.rebecchi@newcastle.ac.uk>
2017-12-04 23:00:00 +0000

    Recursive functions to multiply two numbers, and calculate the square of a number.

--- day4/recursiveMaths.py
+++ day4/recursiveMaths.py
@@ -26,6 +26,38 @@ def factorial(n):
 
     #Otherwise return 0
     return 1
+
+#f(n, 0) = 0
+#f(n, m) = n + f(n, m - 1) || - (n - f(n, m + 1))
+#Create a multiplication function to multiply n and m
+def multiply(n, m):
+
+    #If m is 0 then return 0
+    if m == 0:
+        return 0
+
+    #If m is less than 0 recursively call increasing m
+    if m < 0:
+        return - (n - multiply(n, m + 1))
+
+    #If m is greater than 0 recursively call reducing m
+    if m > 0:
+        return n + multiply(n, m - 1)
+
+#f(0) = 0
+#f(n) = f(n - 1) + 2n - 1
+#Create a function to find the square of a number
+def square(n):
+
+    #If n = 0 then return 0
+    if n == 0:
+        return 0
+
+    #Otherwise return square(n - 1) + 2n - 1
+    return square(n - 1) + (2 * n) - 1
     
 print("\nSum of 1 to 5 = {}".format(sum(5)))
 print("5! = {}".format(factorial(5)))
+print("6 x 4 = {}".format(multiply(6, 4)))
+print("7^2 = {}".format(square(7)))
+
