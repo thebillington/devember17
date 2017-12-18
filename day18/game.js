38cc163f7d230ea4e9fcb8d07e036b3282002c7d
Billy Rebecchi <b.rebecchi@newcastle.ac.uk>
2017-12-18 22:26:06 +0000

    Add basic snake.

--- day18/game.js
+++ day18/game.js
@@ -3,16 +3,42 @@
 // Time finished: 
 
 // Store the grid size
-gridSize = 20;
+var gridSize = 20;
 
 // Canvas size
-cSize = 600;
+var cSize = 600;
+
+// Pixel size
+var pixelSize;
+
+// List to hold the snake
+var snake;
+
+// Snake speed
+var speed;
+
+// Function to return a coordinate
+function Coord(x, y) {
+    
+    // Return the object
+    return {"x": x, "y": y};
+    
+}
 
 // Setup function
 function setup() {
     
     // Create a canvas
     createCanvas(cSize, cSize);
+    
+    // Calculate pixel size
+    pixelSize = width / gridSize;
+    
+    // Create the snake
+    snake = [Coord(2, 0), Coord(1, 0), Coord(0, 0)];
+    
+    // Set the snakes speed
+    speed = {"dx": 1, "dy": 0};
     
 }
 
@@ -22,19 +48,42 @@ function draw() {
     // Draw the grid
     drawGrid();
     
+    // Draw the snake
+    drawSnake();
+    
+}
+
+// Function to drawe a snake
+function drawSnake() {
+    
+    // Draw each of the pixels
+    for (var i = 0; i < snake.length; i++) {
+        
+        console.log(snake[i]);
+        
+        // Get the x and y
+        var x = snake[i].x * pixelSize;
+        var y = snake[i].y * pixelSize;
+        
+        // Fill the pixel
+        color(0);
+        fill(0);
+        quad(x, y, x + pixelSize, y, x + pixelSize, y + pixelSize, x, y + pixelSize);
+        
+    }
+    
 }
 
 // Create a function to render the grid
 function drawGrid() {
     
-    // Calculate pixel size
-    var pixelSize = width / gridSize;
-    
     // Loop
     for (var i = 0; i < gridSize; i++) {
         for(var j = 0; j < gridSize; j++) {
-    
+            
+            // Fill color
             color(0);
+            fill(255);
             
             // Get x and y
             var x = i * pixelSize;
